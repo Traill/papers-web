eb87a3cc923abe405fc9ef73e075ebdabed4960b
Jonas Arnfred <jonas@ifany.org>
2012-11-25 00:13:55 +0100

    Added JSON support

--- src/paper/Paper.scala
+++ src/paper/Paper.scala
@@ -2,8 +2,41 @@ package paper
 
 import net.liftweb.json._
 
+
+// Try to keep this immutable
+case class Paper(val id :       Int, 
+                 val index :    Int,
+                 val title:     Title, 
+                 val authors:   List[Author], 
+                 val abstr:     Abstract, 
+                 val body:      Body, 
+                 val refs:      List[Reference], 
+                 val meta:      Map[String, String],
+                 val links :    List[Link]) extends AbstractPaper {
+
+
+
+}
+
+case class Title(t: String)
+
+case class Author(name: String)
+
+case class Abstract(text: String)
+
+case class Body(text: String)
+
+case class Reference(authors: List[Author], title: Title)
+
+case class Link(index : Int, weight : Int)
+
+
+
+
 // Singleton with some utility functions
 object Paper {
+
+  val empty = Paper(0, 0, Title(""), List(), Abstract(""), Body(""), List(), Map.empty, List())
 
 
   // Convert Paper to Json
@@ -21,6 +54,60 @@ object Paper {
                                JField("authors", JArray(r.authors.map(a => JString(a.name)))),
                                JField("title", JString(r.title.t))
   ))
+
+
+  // Convert JSON to paper
+  def fromJSON(json : String) : Paper = fromJSON(parse(json))
+  def fromJSON(json : JValue) : Paper = {
+
+    def fields(fs : List[JField], p : Paper) : Paper = fs match {
+      case Nil                                    => p
+      case JField("id", JInt(t)) :: rest          => fields(rest, p.setId(t.toInt))
+      case JField("index", JInt(t)) :: rest       => fields(rest, p.setIndex(t.toInt))
+      case JField("title", JString(t)) :: rest    => fields(rest, p.setTitle(t))
+      case JField("authors", JArray(t)) :: rest   => fields(rest, p.setAuthors(authors(t)))
+      case JField("abstract", JString(t)) :: rest    => fields(rest, p.setAbstract(t))
+      case JField("body", JString(t)) :: rest     => fields(rest, p.setBody(t))
+      case JField("refs", JArray(t)) :: rest      => fields(rest, p.setReferences(refs(t)))
+      case JField("meta", JObject(t)) :: rest     => fields(rest, meta(t, p))
+      case JField("links", JArray(t)) :: rest     => fields(rest, p.setLinks(links(t)))
+      case other                                  => throw new Exception("No match for field: " + other)
+    }
+
+    // Extracts Authors from JSON
+    def authors(as : List[JValue]) : List[Author] = as match {
+      case Nil                  => Nil
+      case JString(t) :: rest   => Author(t) :: authors(rest)
+      case otherwise            => throw new Exception("Authors have to be a JString")
+    }
+
+    // Extract References from JSON
+    def refs(rs : List[JValue]) = rs.map(r => ref(r))
+    def ref(r : JValue) = r match {
+      case JObject(List(JField("authors", JArray(as)), JField("title", JString(title))))    => Reference(authors(as), Title(title))
+      case otherwise                                                                        => throw new Exception("malformed link: " + otherwise)
+    }
+
+    // Extract Links from JSON
+    def links(ls : List[JValue]) = ls.map(l => link(l))
+    def link(l : JValue) = l match {
+      case JObject(List(JField("id", JInt(id)), JField("w", JInt(weight)))) => Link(id.toInt, weight.toInt)
+      case otherwise                                                        => throw new Exception("malformed link: " + otherwise)
+    }
+
+    // Extract Meta from JSON
+    def meta(ms : List[JField], p : Paper) : Paper = ms match {
+      case Nil                                  => p
+      case JField(key, JString(value)) :: rest  => meta(rest, p.setMeta(key -> value))
+      case otherwise                            => throw new Exception("Wrong meta format: " + otherwise)
+    }
+
+    // Check if we have a JObject
+    json match {
+      case JObject(fs)  => fields(fs, empty)
+      case otherwise    => throw new Exception("Can't parse JSON " + otherwise)
+    }
+  }
 
 
   // Try to clean the data a little
@@ -52,6 +139,8 @@ abstract class AbstractPaper {
   def setMeta(m : (String, String)) : Paper = 
     Paper(id, index, title, authors, abstr, body, refs, meta + m, links)
 
+  def setTitle(t : String) : Paper =
+    Paper(id, index, Title(t), authors, abstr, body, refs, meta, links)
   def setTitle(t : Title) : Paper =
     Paper(id, index, t, authors, abstr, body, refs, meta, links)
 
@@ -69,41 +158,16 @@ abstract class AbstractPaper {
   def setLinks(newLinks : List[Link]) : Paper = 
     Paper(id, index, title, authors, abstr, body, refs, meta, newLinks)
     
+  def setAbstract(newAbstract : String) : Paper = 
+    Paper(id, index, title, authors, Abstract(newAbstract), body, refs, meta, links)
   def setAbstract(newAbstract : Abstract) : Paper = 
     Paper(id, index, title, authors, newAbstract, body, refs, meta, links)
     
+  def setBody(newBody : String) : Paper = 
+    Paper(id, index, title, authors, abstr, Body(newBody), refs, meta, links)
   def setBody(newBody : Body) : Paper = 
     Paper(id, index, title, authors, abstr, newBody, refs, meta, links)
     
   def setReferences(newRefs : List[Reference]) : Paper = 
     Paper(id, index, title, authors, abstr, body, newRefs, meta, links)
 }
-
-
-
-// Try to keep this immutable
-case class Paper(val id :       Int, 
-                 val index :    Int,
-                 val title:     Title, 
-                 val authors:   List[Author], 
-                 val abstr:     Abstract, 
-                 val body:      Body, 
-                 val refs:      List[Reference], 
-                 val meta:      Map[String, String],
-                 val links :    List[Link]) extends AbstractPaper {
-
-
-
-}
-
-case class Title(t: String)
-
-case class Author(name: String)
-
-case class Abstract(text: String)
-
-case class Body(text: String)
-
-case class Reference(authors: List[Author], title: Title)
-
-case class Link(index : Int, weight : Int)
