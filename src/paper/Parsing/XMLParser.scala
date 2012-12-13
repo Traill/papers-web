44b5163deed16e239c71fdbe6fb52d40f8de1bdc
Jonas Arnfred <jonas@ifany.org>
2012-12-13 23:06:42 +0100

    Made some changes to Louvain clustering

--- src/paper/Parsing/XMLParser.scala
+++ src/paper/Parsing/XMLParser.scala
@@ -46,10 +46,45 @@ trait XMLParser extends TitleExtractor1
    private val extractionOrder: List[(Paper, XMLDocument, List[XMLParagraph]) => (List[XMLParagraph], Paper)] = List(extractTitle, extractAuthors, extractAbstract, extractBody, extractReferences)
 	
 
+   private def cleanUnicode(s : String) : String = {
+     s.replace("\u0001","")
+      .replace("\u0002","")
+      .replace("\u0003","")
+      .replace("\u0004","")
+      .replace("\u0005","")
+      .replace("\u0006","")
+      .replace("\u0007","")
+      .replace("\u0008","")
+      .replace("\u0009","")
+      .replace("\u000a","")
+      .replace("\u000b","")
+      .replace("\u000c","")
+      .replace("\u000d","")
+      .replace("\u000e","")
+      .replace("\u000f","")
+      .replace("\u0010","")
+      .replace("\u0011","")
+      .replace("\u0012","")
+      .replace("\u0013","")
+      .replace("\u0014","")
+      .replace("\u0015","")
+      .replace("\u0016","")
+      .replace("\u0017","")
+      .replace("\u0018","")
+      .replace("\u0019","")
+      .replace("\u001a","")
+      .replace("\u001b","")
+      .replace("\u001c","")
+      .replace("\u001d","")
+      .replace("\u001e","")
+      .replace("\u001f","")
+      .replace("\uffff","")
+   }
+
    // This method returns the xml representation of the text contained in the Source object
    private def getXMLObject(in: Source): Option[Elem] = {
 	  // String generation and illegal xml characters removing
-	  val text = in.mkString.replace("" + '\uffff', "").replace("" + "\u001f", "")
+	  val text : String = cleanUnicode(in.mkString)
       // This instruction is important, otherwise the xml file can't be deleted
       in.close
       Some(XML.loadString("""</?[bi]>""".r.replaceAllIn(text, "")))
