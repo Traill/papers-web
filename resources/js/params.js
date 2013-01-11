bcf7852ebddb2c96d03d7eaa84c3e9ad5dad4b64
Jonas Arnfred <jonas@ifany.org>
2013-01-11 14:13:45 +0000

    Changed initial zoom on map

--- resources/js/params.js
+++ resources/js/params.js
@@ -30,9 +30,8 @@ define(function () {
 	config['zoomMin'] = 0.1;
 	config['zoomMax'] = 5;
 	config['nbIncrement'] = 40;
-	config['zoomInit'] = 1.5;
+	config['zoomInit'] = 1;
 
-	
 	// graph force layout size:
 	config['graph_width'] = 1200;
 	config['graph_height'] = 700;
