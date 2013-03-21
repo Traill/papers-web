09184df65b76c6416e5dcf1b35c5072c5d5c1db7
Jonas Arnfred <jonas@ifany.org>
2013-03-21 01:28:38 +0000

    We can now save graph in mongoDB (plus fixed a bug in last commit)

--- src/paper/Document.scala
+++ src/paper/Document.scala
@@ -37,8 +37,6 @@ object Document {
   // Empty paper for initialization
   val emptyPaper = Paper(Title(""), List(), Abstract(""), Body(""), List())
 
-
-
   // Implicit values so we can write out a paper
   implicit val formats = DefaultFormats
 
@@ -47,82 +45,7 @@ object Document {
 
 
   // Convert JSON to paper
-  def fromJSON(json : String) : Document = fromJSON(parse(json))
-  def fromJSON(json : JValue) : Document = {
-
-    // Parse document from JSON
-    def doc(fs : List[JField], d : Document) : Document = fs match {
-      case Nil                                      => d
-      case JField("paper", JObject(p)) :: rest      => doc(rest, d.setPaper(paper(p, emptyPaper)))
-      case JField("links", JArray(ls)) :: rest      => doc(rest, d.setLinks(links(ls)))
-      case JField("meta", JObject(m)) :: rest       => doc(rest, meta(m, d))
-      case JField("id", JString(t)) :: rest         => doc(rest, d.setId(t))
-      case JField("cluster", JObject(m)) :: rest    => doc(rest, cluster(m, d))
-      case other                                    => throw new Exception("No match in document for field: " + other)
-    }
-
-
-    def paper(fs : List[JField], p : Paper) : Paper = fs match {
-      case Nil                                    => p
-      case JField("title", JObject(t)) :: rest    => paper(rest, p.setTitle(text(t)))
-      case JField("authors", JArray(t)) :: rest   => paper(rest, p.setAuthors(authors(t)))
-      case JField("abstr", JObject(t)) :: rest    => paper(rest, p.setAbstract(text(t)))
-      case JField("body", JObject(t)) :: rest     => paper(rest, p.setBody(text(t)))
-      case JField("refs", JArray(t)) :: rest      => paper(rest, p.setReferences(refs(t)))
-      case other                                  => throw new Exception("No match in paper for field: " + other)
-    }
-
-
-    // Getting text out of title, abstract and body
-    def text(fs : List[JField]) = fs match {
-      case JField("text", JString(t)) :: rest        => t
-      case other                                  => throw new Exception("No match in textfield (title, abstract, body) for field: " + other)
-    }
-
-    // Extracts Authors from JSON
-    def authors(as : List[JValue]) : List[Author] = as match {
-      case Nil                                          => Nil
-      case JObject(JField(_,JString(t)) :: _) :: rest   => Author(t) :: authors(rest)
-      case other                                        => throw new Exception("Authors have to be a JString: " + other)
-    }
-
-    // Extract References from JSON
-    def refs(rs : List[JValue]) = rs.map(r => ref(r))
-    def ref(r : JValue) = r match {
-      case JObject(List(JField("authors", JArray(as)), JField("title", JObject(t))))    => Reference(authors(as), Title(text(t)))
-      case otherwise                                                                    => throw new Exception("malformed reference: " + otherwise)
-    }
-
-    // Extract Links from JSON
-    def links(ls : List[JValue]) = ls.map(l => link(l))
-    def link(l : JValue) = l match {
-      case JObject(List(JField("id", JString(id)), JField("weight", JInt(weight)))) => Link(id, weight.toInt)
-      case otherwise                                                           => throw new Exception("malformed link: " + otherwise)
-    }
-
-    // Extract Meta from JSON
-    def meta(ms : List[JField], d : Document) : Document = ms match {
-      case Nil                                  => d
-      case JField("jsonClass",_) :: rest        => meta(rest, d)
-      case JField(key, JString(value)) :: rest  => meta(rest, d.setMeta(key -> value))
-      case otherwise                            => throw new Exception("Wrong meta format: " + otherwise)
-    }
-
-    // Extract Cluster from JSON
-    def cluster(ms : List[JField], d : Document) : Document = ms match {
-      case Nil                                  => d
-      case JField("jsonClass",_) :: rest        => cluster(rest, d)
-      case JField(key, JString(value)) :: rest  => cluster(rest, d.setCluster(key -> value.toInt))
-      case otherwise                            => throw new Exception("Wrong cluster format: " + otherwise)
-    }
-
-    // Check if we have a JObject
-    json match {
-      case JObject(fs)  => doc(fs, emptyDoc)
-      case otherwise    => throw new Exception("Can't parse JSON " + otherwise)
-    }
-  }
-
+  def fromJSON(json : String) : Document = Serialization.read[Document](json)
 
   // Try to clean the data a little
   //def clean(p : Paper) = Paper(p.title, p.authors.filter(a => a.name.length > 4), p.abstr, p.body, cleanRefs(p.refs), p.meta, p.links)
