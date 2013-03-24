d3296e22b974b50eb7c94fd680eff8e3ae527367
Jonas Arnfred <jonas@ifany.org>
2013-03-24 09:00:55 +0000

    Forgot to add a file

---
+++ src/paper/Parsing/ISIT2012.scala
@@ -0,0 +1,19 @@
+package paper
+
+import java.io.File
+import scala.io.Source
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+
+trait ISIT2012 extends XMLParser with PDFLoader {
+
+  def parseDoc(doc : Document) : Option[Document] = {
+    var paper : Option[Paper] = None
+    val pdfName : String = Analyzer.filePath + doc.id + ".pdf"
+    println(pdfName)
+    val pdfFile : File = new File(pdfName)
+    if (pdfFile.exists) paper = parseFile(doc, pdfToXML(pdfFile))
+    return paper map { p => doc.setPaper(p) }
+  }
+
+}
